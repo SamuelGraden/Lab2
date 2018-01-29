13955adb0c59a8ea26ad96dde7712b0f683ffe6f
Samuel Gradén <samuel.graden@gmail.com>
2018-01-29 15:04:36 +0100

    Första "room"
    Signed-off-by: Samuel Gradén <samuel.graden@gmail.com>

--- Lab2/src/lab2/level/Room.java
+++ Lab2/src/lab2/level/Room.java
@@ -1,5 +1,34 @@
 package lab2.level;
 
-public class Room {
+import java.awt.Color;
 
+public class Room {
+	private Color color;
+	private int dx;
+	private int dy;
+	
+	private Room northRoom;
+	private Room eastRoom;
+	private Room southRoom;
+	private Room westRoom;
+	
+	public Room(Color color, int dx, int dy) {
+		this.color = color;
+		this.dx = dx;
+		this.dy = dy;
+			
+	}
+	
+	public void 	connectNorthTo(Room r) {
+		this.northRoom = r;
+	}
+	public void connectWestTo(Room r) {
+		this.westRoom = r;
+	}
+	public void connectEastTo(Room r) {
+		this.eastRoom = r;
+	}
+	public void connectSouthTo(Room r) {
+		this.southRoom = r;
+	}
 }
