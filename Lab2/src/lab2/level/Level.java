fb5a3e2718f7dca75ed35f3f65dbda18a73011fa
Anton Johansson <anton.Frappe@outlook.com>
2018-01-29 14:41:45 +0100

    Skapade alla filer
    Signed-off-by: Anton Johansson <anton.Frappe@outlook.com>

---
+++ Lab2/src/lab2/level/Level.java
@@ -0,0 +1,5 @@
+package lab2.level;
+
+public class Level {
+
+}
---
+++ Lab2/src/lab2/level/LevelGUI.java
@@ -0,0 +1,10 @@
+package lab2.level;
+
+import java.util.Observer;
+
+public class LevelGUI extends Observer {
+	public LevelGUI(Level level, String name) {
+		
+	}
+
+}
