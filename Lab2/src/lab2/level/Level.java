a81f652681acf7b955a1c42fc7e301902df75c7c
Anton Johansson <anton.Frappe@outlook.com>
2018-01-29 15:36:13 +0100

    Litet i LevelGUI och Level
    Signed-off-by: Anton Johansson <anton.Frappe@outlook.com>

--- Lab2/src/lab2/level/Level.java
+++ Lab2/src/lab2/level/Level.java
@@ -1,5 +1,28 @@
 package lab2.level;
 
-public class Level {
+import java.util.ArrayList;
+import java.util.Observable;
 
+public class Level extends Observable {
+	ArrayList<Room> roomList;
+	private Room r;
+	private int x;
+	private int y;
+	
+	public Level() {
+		roomList=new ArrayList<Room>();
+		
+	}
+	
+	public boolean place(Room r, int x, int y)  {
+		this.r = r;
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void firstLocation(Room r) {
+			
+	}
+	
 }
+
--- Lab2/src/lab2/level/Room.java
+++ Lab2/src/lab2/level/Room.java
@@ -7,19 +7,64 @@ public class Room {
 	private int dx;
 	private int dy;
 	
+	private int posX;
+	private int posY;
+	
 	private Room northRoom;
 	private Room eastRoom;
 	private Room southRoom;
 	private Room westRoom;
 	
-	public Room(Color color, int dx, int dy) {
+	public Room(Color color, int dx, int dy, int posX, int posY) {
 		this.color = color;
 		this.dx = dx;
 		this.dy = dy;
+		this.posX = posX;
+		this.posY = posY;
 			
 	}
 	
-	public void 	connectNorthTo(Room r) {
+	public Color getColor() {
+		return color;
+	}
+
+	public int getDx() {
+		return dx;
+	}
+
+	public int getDy() {
+		return dy;
+	}
+
+	public int getPosX() {
+		return posX;
+	}
+
+	public int getPosY() {
+		return posY;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public int getDx() {
+		return dx;
+	}
+
+	public int getDy() {
+		return dy;
+	}
+
+	public int getPosX() {
+		return posX;
+	}
+
+	public int getPosY() {
+		return posY;
+	}
+
+	public void connectNorthTo(Room r) {
 		this.northRoom = r;
 	}
 	public void connectWestTo(Room r) {
