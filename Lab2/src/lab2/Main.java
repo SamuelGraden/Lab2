c42231fb5559957e02c90f41baf0f1767e33ccf1
Samuel Gradén <samuel.graden@gmail.com>
2018-01-29 13:56:10 +0100

    DagensEvent
    Signed-off-by: Samuel Gradén <samuel.graden@gmail.com>

--- Lab2/src/lab2/Main.java
+++ Lab2/src/lab2/Main.java
@@ -3,7 +3,7 @@ package lab2;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Jag är en dum Programmerare\n//Samuel");
+		System.out.println("Jag är en dum Programmerare\n//Samuel\nTydligen inte med tanke på dagens event");
 
 	}
 
