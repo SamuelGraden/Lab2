8de231a748ac8534ea25aaa01fa50c5492d933fe
Anton Johansson <anton.Frappe@outlook.com>
2018-01-30 13:20:04 +0100

    Da wae
    Signed-off-by: Anton Johansson <anton.Frappe@outlook.com>

--- Lab2/src/lab2/level/Level.java
+++ Lab2/src/lab2/level/Level.java
@@ -6,14 +6,17 @@ import java.util.Observable;
 
 public class Level extends Observable {
 	private ArrayList<Room> roomList;
+	private boolean placeactive=true;
 	
 	public Level() {
 		roomList=new ArrayList<Room>();
 	}
 	
 	public boolean place(Room r, int x, int y)  {
+		if(!placeactive)return false;
 		int dimX=r.getDx()/2;
 		int dimY=r.getDy()/2;
+		//Tests if the middle of the "to be placed room" is within the area of all of previous rooms + half of the placed room.
 		boolean placement=true;
 		for(int i=0;i<roomList.size();i++) {
 			if(((roomList.get(i).getPosX()-dimX)<(x+dimX) && (roomList.get(i).getPosX()+roomList.get(i).getDx()+dimX)<(x+dimX))) {
@@ -32,8 +35,10 @@ public class Level extends Observable {
 	}
 	
 	public void firstLocation(Room r) {
-			
+		
+		placeactive=false;
 	}
+	
 	public ArrayList<Room> getRoomList() {
 		return this.roomList;
 	}
